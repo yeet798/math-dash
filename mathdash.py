d373ad4c254d8483fdea09f11953ca8ab9eed193
siddw01 <siddw01@gmail.com>
2020-01-19 12:27:16 -0600

    First Push

--- mathdash.py
+++ mathdash.py
@@ -1,12 +1,33 @@
 import pygame, sys
 from pygame.locals import *
 
+
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!' )
+
+res = (1200, 600)
+
+pygame.mixer.music.load('blue_sky.mp3')
+pygame.mixer.music.play(-1)
+
+DISPLAYSURF = pygame.display.set_mode(res)
+pygame.display.set_caption('Math Dash')
+
+clock = pygame.time.Clock()
+player_token = pygame.image.load('player_token.png')
+
+def player(x,y):
+    DISPLAYSURF.blit(player_token, (x,y))
+
+x = (res[0] * .15)
+y = (res[1] * .75)
+
 while True: # main game loop 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            sys.exit()
+            quit()
+
+    player(x, y)
+
     pygame.display.update()
+    clock.tick(60)
